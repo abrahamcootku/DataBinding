ba77f0eb190e03866443fc6df98a209ae68ca2c4
Abraham Samuel Coot Ku <23470124@campeche.tecnm.mx>
2025-10-29 11:52:23 -0600

    agrega enlace de datos

--- DataBinding.Xaml/MainPage.xaml.cs
+++ DataBinding.Xaml/MainPage.xaml.cs
@@ -7,12 +7,6 @@
         public MainPage()
         {
             InitializeComponent();
-            EnteredTextLabel.Text = string.Empty;
-        }
-
-        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            EnteredTextLabel.Text = TextEntry.Text;
         }
     }
 }
