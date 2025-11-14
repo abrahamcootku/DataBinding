ac65b2bf4a70b8352c0e093b43daf9ea5a8e3636
Abraham Samuel Coot Ku <23470124@campeche.tecnm.mx>
2025-11-14 10:36:15 -0600

    ejemplo de DataBinding a una coleccion de objetos

--- Data.BindingColeccion/Models/OrigenDePaquete.cs
+++ Data.BindingColeccion/Models/OrigenDePaquete.cs
@@ -1,15 +1,72 @@
-﻿namespace DataBindingColeccion.Models
+﻿using System.ComponentModel;
+
+namespace DataBindingColeccion.Models
 {
-     class OrigenDePaquete
-     {
-        public string Nombre { get; set; } = string.Empty;
-        public string Origen { get; set; } = string.Empty;
-        public bool EstaHabilitado { get; set; } = false;
-        
+    public class OrigenDePaquete : INotifyPropertyChanged
+    {
+        //public string Nombre { get; set; } = string.Empty;
+        //public string Origen { get; set; } = string.Empty;
+        //public bool EstaHabilitado { get; set; } = false;
+
+        private string? _nombre = string.Empty;
+        private string? _origen = string.Empty;
+        private bool? _estaHabilitado = false;
+
+        public string? Nombre
+        {
+            get => _nombre;
+            set
+            {
+                if (_nombre != value)
+                {
+                    _nombre = value;
+                    OnPropertyChanged(nameof(Nombre));
+                    
+                }
+            }
+        }
+
+        public string? Origen
+        {
+            get => _origen;
+            set
+            {
+                if (_origen != value)
+                {
+                    _origen = value;
+                    OnPropertyChanged(nameof(Origen));
+                }
+            }
+        }
+
+        public bool? estaHabilitado
+        {
+            get => _estaHabilitado;
+            set
+            {
+                if (estaHabilitado != value)
+                {
+                    _estaHabilitado = value;
+                    OnPropertyChanged(nameof(estaHabilitado));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+
         public override string ToString()
         {
-            return $"{ Nombre} - ({Origen})";
+            return $"{Nombre} - ({Origen})";
         }
-     }
 
+
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(
+                this, new PropertyChangedEventArgs(propertyName)
+                );
+        }
+
+
+    }
 }
